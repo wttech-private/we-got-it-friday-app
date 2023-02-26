21d82560564c3036b77a5b0ae7a6d2deea455c09
Karol Kujawski <karol.kujawski@wundermanthompson.com>
2023-02-26 19:04:58 +0100

    Starting point vol-2

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -1,18 +1,12 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    reporter: 'cypress-mochawesome-reporter',
-    screenshotsFolder: 'reports/screenshots',
     viewportWidth: 1200,
-    baseUrl: "https://d39x7kisu5mvy0.cloudfront.net/",
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
       config.video = false;
       return config;
     },
-  },
-  env: {
-    apiUrl: "https://0bortdj0g9.execute-api.eu-central-1.amazonaws.com"
   }
 });
