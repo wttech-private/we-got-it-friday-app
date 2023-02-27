822233f2cf4069ea177fbfb3c3b2cfcbe2dc80eb
Karol Kujawski <karol.kujawski@wundermanthompson.com>
2023-02-27 21:54:07 +0100

    Fix allure

--- tests/cypress/config/int.ts
+++ tests/cypress/config/int.ts
@@ -4,6 +4,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 export default defineConfig({
   e2e: {
     viewportWidth: 1200,
+    screenshotsFolder: "build/cypress/screenshots",
     baseUrl: "https://dwwsfgcsv3q4k.cloudfront.net",
     setupNodeEvents(on, config) {
       allureWriter(on, config);
@@ -12,6 +13,7 @@ export default defineConfig({
     },
     env: {
       allure: true,
+      allureResultsPath: "build/allure-results",
       apiUrl: "https://zcg6pnou4b.execute-api.eu-central-1.amazonaws.com"
     }
   }
--- tests/cypress/config/prod.ts
+++ tests/cypress/config/prod.ts
@@ -4,6 +4,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 export default defineConfig({
   e2e: {
     viewportWidth: 1200,
+    screenshotsFolder: "build/cypress/screenshots",
     baseUrl: "https://d39x7kisu5mvy0.cloudfront.net/",
     setupNodeEvents(on, config) {
       allureWriter(on, config);
@@ -12,6 +13,7 @@ export default defineConfig({
     },
     env: {
       allure: true,
+      allureResultsPath: "build/allure-results",
       apiUrl: "https://0bortdj0g9.execute-api.eu-central-1.amazonaws.com"
     }
   }
